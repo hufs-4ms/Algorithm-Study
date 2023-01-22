090ca9733751c64e8ef3d5c6a09042db028c7cb4
htoh702 <htoh702@gmail.com>
2023-01-22 18:47:13 +0900

    2023년_1월_3주차

---
+++ ohhong/2023년_1월_3주차/baekjun_11478/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        String str = st.nextToken();
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<=str.length();j++){
+                set.add(str.substring(i,j));
+            }
+        }
+        bw.write(set.size()+"\n");
+        bf.close();
+        bw.flush();
+        bw.close();
+    }
+}
