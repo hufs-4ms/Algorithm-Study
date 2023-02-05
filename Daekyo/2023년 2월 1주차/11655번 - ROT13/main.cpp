0470a329075ac3d2ad38be4c396429e32d4182dc
JUNGDAEKYO <best9631@naver.com>
2023-02-05 22:42:31 +0900

    23년_2월_1주차

---
+++ Daekyo/2023년 2월 1주차/11655번 - ROT13/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+string s;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	getline(cin, s);
+	
+	for(int i=0; i<s.size(); i++){
+		if(s[i]>=65&&s[i]<=90){
+			if(s[i]+13>90){
+				s[i] = s[i] + 13 - 26;
+			}
+			else s[i] = s[i] + 13;
+		}
+		
+		if(s[i]>=97&&s[i]<=122){
+			if(s[i]+12>122){
+				s[i] = s[i] + 13 - 26;
+			}
+			else s[i] = s[i] + 13;
+		}
+		
+		cout<< s[i];
+	}
+	
+	return 0;
+	
+} 
